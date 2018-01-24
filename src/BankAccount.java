95a31914cd30d5069ba8a714a7a8d94c25c0dae8
Josh Harkema <josh@joshharkema.com>
2018-01-24 13:30:25 -0700

    Passes all the tests.

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -3,11 +3,15 @@ public class BankAccount {
     double overdraftAmount = 100;
 
     public void deposit(double amount) {
-
+        if (amount > 0) {
+            this.balance += amount;
+        }
     }
 
     public void withdraw(double amount) {
-
+        if (amount > 0 && (this.balance + this.overdraftAmount) - amount >= 0) {
+            this.balance -= amount;
+        }
     }
 
     public double getBalance() {
@@ -16,6 +20,10 @@ public class BankAccount {
 
     public void setOverdraftAmount(double amount) {
         this.overdraftAmount = amount;
+
+        if (this.overdraftAmount - amount < 0) {
+            this.overdraftAmount = 0.0;
+        }
     }
 
     public static void main(String[] args) {
