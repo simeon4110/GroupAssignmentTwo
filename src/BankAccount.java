7a631176cd873b65a921e2e364a49f35585ea979
Josh Harkema <josh@joshharkema.com>
2018-01-24 13:10:15 -0700

    Added the framework, instance variables, and setter/getter methods.

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,24 @@
 public class BankAccount {
+    double balance;
+    double overdraftAmount = 100;
+
+    private void deposit(double amount) {
+
+    }
+
+    private void withdrawl(double amount) {
+
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setOverdraftAmount(double amount) {
+        this.overdraftAmount = amount;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
