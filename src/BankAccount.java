e8b8cc805868d160dd20ea6d42b10fdbf877dcca
Josh Harkema <josh@joshharkema.com>
2018-01-24 13:35:28 -0700

    Added javadocs.

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,32 +1,50 @@
+/**
+ * Class is a simple bank account with balance and overdraftAmount instance variables.
+ */
 public class BankAccount {
     double balance;
     double overdraftAmount = 100;
 
+    /**
+     * Allows an arbitrary double to be added to the balance instance variable.
+     *
+     * @param amount The amount to be deposited.
+     */
     public void deposit(double amount) {
         if (amount > 0) {
             this.balance += amount;
         }
     }
 
+    /**
+     * Verifies withdrawls against the user's available balance + overdraftAmount.
+     * @param amount The amount to withdraw.
+     */
     public void withdraw(double amount) {
         if (amount > 0 && (this.balance + this.overdraftAmount) - amount >= 0) {
             this.balance -= amount;
         }
     }
 
+    /**
+     * Getter method returns the user's balance.
+     * @return The user's balance as a double.
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Sets user's overdraft amount.
+     * @param amount The desired amount as a double.
+     */
     public void setOverdraftAmount(double amount) {
         this.overdraftAmount = amount;
 
         if (this.overdraftAmount - amount < 0) {
             this.overdraftAmount = 0.0;
         }
-    }
-
-    public static void main(String[] args) {
 
     }
+
 }
